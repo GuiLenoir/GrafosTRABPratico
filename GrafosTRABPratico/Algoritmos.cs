04a883a86f3394a693afd42cab3e7690e4561824
Guilherme Lenoir <guilhermelenoir@gmail.com>
2025-12-05 14:06:35 -0300

    começo da classe algoritmo e implementação

---
+++ GrafosTRABPratico/Algoritmos.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrafosTRABPratico
+{
+    public class Algoritmos 
+    {
+
+        //TEM QUE VERIFICAR PRA VER O QUE CADA ALGORTIMO REALMENTE VAI RETORNAR (SE RETORNA UM INT COM INFORMAÇÃO ESPECIFICA OU UMA STRING COM VÁRIAS INFORMAÇÕES POR EXEMPLO)
+
+        //AQUI VAI TER TODOS OS ALGORITMOS QUE A GENTE TALVEZ USE (DIJKSTRA E BELLMAN SÃO OS 2 PRA CAMINHO MINIMO, MAS DEPENDENDO DO GRAFO VAI VARIAR QUAL DOS DOIS VAI USAR)
+        public void Dijkstra()
+        {
+
+        }
+
+        public void BellmanFord()
+        {
+
+        }
+
+        public void Kruskal()
+        {
+
+        }
+        public void Prim()
+        {
+
+        }
+        //tem que ver qual vai usar pra coloração (botei nome generico)
+        public void Coloracao()
+        {
+
+        }
+
+        public void CircuitoEuleriano()
+        {
+
+        }
+        public void CircuitoHamiltoniano()
+        {
+
+        }
+        //talvez ainda precise de mais
+    }
+}
